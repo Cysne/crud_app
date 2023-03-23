12eea85dd54ec8214a57c6f3d5ae37bb80607165
liziane cysne <liziane.medeiros@sefin.fortaleza.ce.gov.br>
2023-03-23 09:46:51 -0300

    adicionando o bootstrap

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,4 @@
+  //= link bootstrap.min.js
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,5 +1,7 @@
 # Pin npm packages by running ./bin/importmap
-
+pin"bootstrap", to: "bootstrap.min.js", preload: true
+pin "jquery", to: "jquery.min.js", preload: true
+pin "popper.js", to: "popper.min.js", preload: true
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
